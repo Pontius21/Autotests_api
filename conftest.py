a76808b6ea144baa97b54428647098532796c5f2
Максим <maks-panteleev-98@mail.ru>
2024-06-28 17:49:31 +0300

    added auto-selection of the id of the "My Documents" directory in the clear_directory method

--- conftest.py
+++ conftest.py
@@ -3,11 +3,6 @@ import pytest
 from api.Auth_api import Auth
 from api.DocumentDirectory_api import DocumentDirectory
 from assertion import assert_status_code
-import configparser
-
-parser = configparser.ConfigParser()
-parser.read('config.ini')
-id_my_documents = parser.get('config', 'id_my_documents')
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -30,8 +25,8 @@ def directory_id_my_documents():
 
 
 @pytest.fixture(scope="function", autouse=True)
-def сlear_directory():
+def сlear_directory(directory_id_my_documents):
     # очистка директории "Мои документы"
     yield
-    response = DocumentDirectory.сlear_directory(id_my_documents)
+    response = DocumentDirectory.сlear_directory(directory_id_my_documents)
     assert_status_code(response)
